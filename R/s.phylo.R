0c4aef83ed2d4bedb9edbb0a187aa0c1651dc08a
vitaliskim <vitaliskim@gmail.com>
2021-10-18 02:07:27 +0200

    Updated the description of s.phylo

--- R/s.phylo.R
+++ R/s.phylo.R
@@ -1,6 +1,7 @@
 #' Species phylogeny generator
 #'
-#' Given a species-by-site matrix (community), this function:
+#' This  function generates the phylogeney of species and plots the phylogenetic tree. In
+#'  particular, given a species-by-site matrix (community), \link[msco]{s.phylo}:
 #' * uses the \link[taxize]{tax_name} function to obtain (from the
 #'  [NCBI](https://www.ncbi.nlm.nih.gov/) or [ITIS](https://www.itis.gov/) online
 #'   databases) the genus and family taxa levels of species in the community.
